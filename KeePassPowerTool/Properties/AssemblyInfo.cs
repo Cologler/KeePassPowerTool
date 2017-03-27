19d3daa0da5c28b99a83288be7446cbc25072491
赖巍 <skyoflw@gmail.com>
2017-03-27 15:11:07 +0800

    init plugin

--- KeePassPowerTool/Properties/AssemblyInfo.cs
+++ KeePassPowerTool/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeePassPowerTool")]
+[assembly: AssemblyProduct("KeePass Plugin")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
